6cd1d8c498a247e455c5eb8c43725233e2263de5
Dmitry <bratchikovdy@gmail.com>
2021-04-17 21:45:44 +0300

    фиксы фронта небольшие

--- studygram/settings.py
+++ studygram/settings.py
@@ -126,4 +126,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR), 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 INTERNAL_IPS = ['127.0.0.1']
