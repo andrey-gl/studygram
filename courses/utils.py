eebe90382c1a6f3039b0369a22891851539cefab
Dmitry <bratchikovdy@gmail.com>
2021-04-19 23:19:36 +0300

    Поменял тип передаваемых данных в фильтры на массив словарей

--- courses/utils.py
+++ courses/utils.py
@@ -1,5 +1,10 @@
 def get_user_list(queryset):
     list_final = []
     for item in queryset:
-        list_final.append(str(item.last_name) + str(item.first_name))
+        list_final.append(
+            {
+                'id': str(item.id),
+                'name': str(item.first_name),
+                'surname': str(item.last_name)
+            })
     return list_final
